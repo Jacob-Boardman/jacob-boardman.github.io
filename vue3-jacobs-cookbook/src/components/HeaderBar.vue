ff6793ef5cab9f8d1b259cf2e6dffc4e350f2cea
Jacob Boardman <jacob.boardman.jb@gmail.com>
2023-04-28 18:39:10 +0100

    trying to add a nice banner image

--- vue3-jacobs-cookbook/src/components/HeaderBar.vue
+++ vue3-jacobs-cookbook/src/components/HeaderBar.vue
@@ -12,20 +12,20 @@ export default {
   data() {
     return {
       notBasePath: {
-        '/': false,
-        '/contact-page': true,
-        '/breakfast-page': true,
-        '/lunch-page': true,
-        '/dinner-page': true,
-        '/dessert-page': true
-      }
+        "/": false,
+        "/contact-page": true,
+        "/breakfast-page": true,
+        "/lunch-page": true,
+        "/dinner-page": true,
+        "/dessert-page": true,
+      },
     };
-  }
+  },
 };
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Kite+One&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Kite+One&display=swap");
 
 .header {
   width: 100vw;
@@ -36,11 +36,14 @@ export default {
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
+  /* background-image: url("../assets/Your-Guide-to-Kitchen-Herbs-and-Spices-Cover.jpg");
+  background-position: bottom 100px;
+  background-size: 100%; */
 }
 
-.header>h1 {
+.header > h1 {
   margin-left: 30px;
-  font-family: 'Kite One', sans-serif;
+  font-family: "Kite One", sans-serif;
   font-size: xxx-large;
 }
 
@@ -53,7 +56,7 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
 
-.header-image>img {
+.header-image > img {
   height: 100%;
   aspect-ratio: 1/1;
 }
